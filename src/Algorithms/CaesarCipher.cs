edbc18bbd734e9e2c41e9de398cc263a0f3a9000
Tim <tim@timcollins.ie>
2015-07-03 20:04:50 +0100

    Added initial version of CaesarCipher and associated unit tests.

---
+++ src/Algorithms/CaesarCipher.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using Domain.Implementation;
+using Domain.Interfaces;
+
+namespace Algorithms
+{
+    public class CaesarCipher
+    {
+        private readonly IConsoleReader _reader;
+        private readonly char[] _punctuation = "!\"#$%&'()*+'-./:;<=>?`{|}~[\\]^_".ToCharArray();
+        private readonly char[] _numbers = "0123456789".ToCharArray();
+
+        public CaesarCipher(IConsoleReader reader)
+        {
+            _reader = reader;
+        }
+
+        public CaesarCipher()
+        {
+            _reader = new ConsoleReader();    
+        }
+
+        public string SolveIt()
+        {
+            int length = Convert.ToInt32(_reader.ReadLine());
+            string input = _reader.ReadLine();
+            int factor = Convert.ToInt32(_reader.ReadLine());
+
+            //var source = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            var inputChars = input.ToCharArray();
+
+            for (int i = 0; i < length; i++)
+            {
+                char c = inputChars[i];
+                if (_punctuation.Contains(c) || _numbers.Contains(c))
+                {
+                    continue;
+                }
+
+                inputChars[i]++;
+                inputChars[i]++;
+            }
+
+            return new string(inputChars);
+        }
+    }
+}
---
+++ src/UnitTests/CaesarCipherTests.cs
@@ -0,0 +1,89 @@
+﻿using Algorithms;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class CaesarCipherTests
+    {
+        [Test]
+        public void BasicAlphabeticalTest()
+        {
+            string[] input =
+            {
+                "3",
+                "abc",
+                "2"
+            };
+
+            string output = new CaesarCipher(MoqUtil.SetupRandMock(input)).SolveIt();
+
+            Assert.AreEqual("cde", output);
+        }
+
+        [Test]
+        public void PunctuationShouldBeIgnored()
+        {
+            // 32 - 47 inclusive
+            // 58 - 64 inclusive
+            // 91 - 96 inclusive
+            // 123 - 126 inclusive
+            string[] input =
+            {
+                "30",
+                "!\"#$%&'()*+-./:;<=>?`{|}~[\\]^_",
+                "2"
+            };
+
+            string output = new CaesarCipher(MoqUtil.SetupRandMock(input)).SolveIt();
+
+            Assert.AreEqual("!\"#$%&'()*+-./:;<=>?`{|}~[\\]^_", output);
+        }
+
+        [Test]
+        public void MixedPunctuationAndLettersShouldBeHandled()
+        {
+            string[] input =
+            {
+                "7",
+                "!^_!qrt",
+                "2"
+            };
+
+            string output = new CaesarCipher(MoqUtil.SetupRandMock(input)).SolveIt();
+
+            Assert.AreEqual("!^_!stv", output);
+        }
+
+        [Test]
+        public void NumbersShouldBeIgnored()
+        {
+            string[] input =
+            {
+                "6",
+                "123456",
+                "2"
+            };
+
+            string output = new CaesarCipher(MoqUtil.SetupRandMock(input)).SolveIt();
+
+            Assert.AreEqual("123456", output);
+        }
+
+        [Test]
+        public void BasicCipher()
+        {
+            string[] input =
+            {
+                "11",
+                "middle-Outz",
+                "2"
+            };
+
+            // Do something
+            string output = new CaesarCipher(MoqUtil.SetupRandMock(input)).SolveIt();
+
+            Assert.AreEqual("okffng-Qwvb", output);
+        }
+    }
+}
